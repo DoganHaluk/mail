82e2734fd3a90b901c20900368d11d81901decee
DoganHaluk <halukdogan78@gmail.com>
2021-12-14 23:49:54 +0100

    email sent is achieved

--- src/main/java/be/vdab/mail/mailing/DefaultLidMailing.java
+++ src/main/java/be/vdab/mail/mailing/DefaultLidMailing.java
@@ -5,7 +5,10 @@ import be.vdab.mail.exceptions.KanMailNietZendenException;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
 
 @Component
 public class DefaultLidMailing implements LidMailing {
@@ -18,12 +21,13 @@ public class DefaultLidMailing implements LidMailing {
     @Override
     public void stuurMailNaRegistratie(Lid lid, String ledenURL) {
         try {
-            var message = new SimpleMailMessage();
-            message.setTo(lid.getEmailAdres());
-            message.setSubject("Geregistreerd");
-            message.setText("Je bent nu lid. Je nummer is:" + lid.getId());
+            var message = sender.createMimeMessage();
+            var helper = new MimeMessageHelper(message);
+            helper.setTo(lid.getEmailAdres());
+            helper.setSubject("Geregistreerd");
+            helper.setText("<h1>Je bent nu lid.</h1>Je nummer is:" + lid.getId(), true);
             sender.send(message);
-        } catch (MailException ex) {
+        } catch (MailException | MessagingException ex) {
             throw new KanMailNietZendenException(ex);
         }
     }
